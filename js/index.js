37caa7311588ec1b028bedcf9b980f654923cd8d
Danilo Tosev <danilo.tosev@tutanota.com>
2024-05-29 12:18:01 +0200

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,69 @@
+function newWidnow1(){
+  
+    window.open("pas.html", "" ,"width=350,height=250");
+}
+
+function newWidnow2(){
+    
+    window.open("guska.html", "" ,"width=350,height=250");
+}
+
+function newWidnow3(){
+    window.open("macka.html", "" ,"width=350,height=250");
+}
+
+function newWidnow4(){
+
+    window.open("pile.html", "" ,"width=350,height=250");
+}
+
+function newWidnow5(){
+    window.open("krava.html", "" ,"width=350,height=250");
+}
+
+
+var audio = new Audio('./audio/pasLaje.mp3');
+function pasLaje(){
+    audio.play();
+}
+
+function pasNeLaje(){
+    audio.pause();
+}
+
+var audio1 = new Audio('./audio/guskaGace.mp3');
+function guskaGace(){
+    audio1.play();
+}
+
+function guskaNeGace(){
+    audio1.pause();
+}
+
+var audio2 = new Audio('./audio/mackaMjauce.mp3');
+function mackaMjauce(){
+    audio2.play();
+}
+
+function mackaNeMjauce(){
+    audio2.pause();
+}
+
+var audio3 = new Audio('./audio/pilePijuce.mp3');
+function pilePijuce(){
+    audio3.play();
+}
+
+function pileNePijuce(){
+    audio3.pause();
+}
+
+var audio4 = new Audio('./audio/kravaMuce.mp3');
+function kravaMuce(){
+    audio4.play();
+}
+
+function kravaNeMuce(){
+    audio4.pause();
+}
+
